e4bb0f0606445d33cb693895599bbc48528666bd
haphut <haphut@mistmap.com>
2022-09-25 02:44:45 +0300

    test: Add test involving import

---
+++ src/util.test.ts
@@ -0,0 +1,9 @@
+import transformUnknownToError from "./util";
+
+test("transformUnknownToError", () => {
+  expect(transformUnknownToError(1)).toStrictEqual(new Error("1"));
+  expect(transformUnknownToError("foo")).toStrictEqual(new Error("foo"));
+  expect(transformUnknownToError(new Error("foo"))).toStrictEqual(
+    new Error("foo")
+  );
+});
